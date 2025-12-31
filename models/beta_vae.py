fix: Update LPIPS weight and adjust loss calculation in BetaVAE; add detailed documentation on LPIPS integration and training dynamics

models/beta_vae.py
@@ -28,7 +28,7 @@ class BetaVAE(BaseVAE):
         for param in self.lpips_model.parameters():
             param.requires_grad = False
         # lpips loss weight, intial 0.1 to 0.5
-        self.lpips_weight = 0.5
+        self.lpips_weight = 1
 
         self.latent_dim = latent_dim
         self.beta = beta
@@ -159,7 +159,7 @@ class BetaVAE(BaseVAE):
         log_var = args[3]
         kld_weight = kwargs['M_N']  # Account for the minibatch samples from the dataset
 
-        recons_loss =F.mse_loss(recons, input)
+        mse_loss =F.mse_loss(recons, input)
 
         # 计算感知损失
         # 确保 self.lpips_model 和 input 在同一个 device 上
@@ -173,16 +173,20 @@ class BetaVAE(BaseVAE):
         
         kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
 
+        recons_loss = mse_loss + self.lpips_weight * perceptual_loss
+
         if self.loss_type == 'H': # https://openreview.net/forum?id=Sy2fzU9gl
-            loss = recons_loss + self.lpips_weight*perceptual_loss + self.beta * kld_weight * kld_loss
+            loss = recons_loss  + self.beta * kld_weight * kld_loss
         elif self.loss_type == 'B': # https://arxiv.org/pdf/1804.03599.pdf
             self.C_max = self.C_max.to(input.device)
             C = torch.clamp(self.C_max/self.C_stop_iter * self.num_iter, 0, self.C_max.data[0])
-            loss = recons_loss + self.lpips_weight*perceptual_loss + self.gamma * kld_weight* (kld_loss - C).abs()
+            loss = recons_loss + self.gamma * kld_weight* (kld_loss - C).abs()
         else:
             raise ValueError('Undefined loss type.')
 
-        return {'loss': loss, 'Reconstruction_Loss':recons_loss, 'KLD_Loss':kld_loss}
+        # return {'loss': loss, 'Reconstruction_Loss':recons_loss, 'KLD_Loss':kld_loss}
+        return {'loss': loss, 'Reconstruction_Loss':self.lpips_weight*perceptual_loss, 'KLD_Loss':kld_loss}
+        
 
     def sample(self,
                num_samples:int,